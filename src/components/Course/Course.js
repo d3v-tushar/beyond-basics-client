61d2fb95b9befdf1a00542bd455b04749bd18525
d3v-tushar <d3v.tushar@gmail.com>
2022-10-26 12:16:34 +0600

    Google & GitHub Login System Added

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 const Course = ({course}) => {
     return (
-        <div className="card card-side bg-base-100 shadow-xl">
+        <div className="card card-side bg-sky-300 shadow-xl">
         <figure><img src="https://placeimg.com/200/280/arch" alt="Movie"/></figure>
         <div className="card-body">
             <h2 className="card-title">{course.course}</h2>
-            <p>Click the button to watch on Jetflix app.</p>
+            <ol id='star'></ol>
+            <p>Course Duration: {course.duration}</p>
+            <p>Course Fee: {course.fee}</p>
             <div className="card-actions justify-end">
-            <button className="btn btn-primary">Watch</button>
+            <button className="btn btn-primary">Learn More</button>
             </div>
         </div>
         </div>
--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import { useEffect } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Course from '../Course/Course';
 import CourseTitle from '../CourseTitle/CourseTitle';
@@ -7,6 +8,42 @@ import './Courses.css'
 const Courses = () => {
     const courses = useLoaderData();
     console.log(courses);
+
+    // const showStar = (courses) =>{
+    //     courses.forEach(element => {
+    //         const initialRate = ((element.rating)+'').split(".");
+    //         let [firstRate, secondRate] = initialRate;
+    //         if(secondRate === undefined){
+    //             secondRate='0';
+    //         }
+    //         const newFirstRate = +firstRate;
+    //         const newSecondRate = +secondRate;
+            
+    //         const ol = document.getElementById('star');
+    //         const li = document.createElement('li');
+    
+    //         for(let i=1; i<= newFirstRate; i++){
+    //             const span = document.createElement('span');
+    //             span.classList.add("star-color");
+    //             span.innerHTML=`<i class="fas fa-star"></i>`;
+    //             li.appendChild(span);
+    //         }
+    //         const span = document.createElement('span');
+    //         span.classList.add("star-color");
+    //         if(newSecondRate >0 && newSecondRate <=5){
+    //             span.innerHTML=`<i class="far fa-star"></i>`;
+    //         }
+    //         else if(newSecondRate >5 && newSecondRate <10){
+    //             span.innerHTML=`<i class="fas fa-star-half-alt"></i>`;
+    //         }
+    //         li.appendChild(span);
+    //         ol.appendChild(li);
+    //     });
+    // }
+
+    // useEffect(() =>{
+    //     showStar(courses);
+    // },[courses])
     return (
         <div className='course-container'>
             <div className='title-container grid gap-2 m-6'>
