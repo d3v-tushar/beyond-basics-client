d0b52abed3c1cf3f0fdc018087154fb220bcc3db
d3v-tushar <d3v.tushar@gmail.com>
2022-10-27 11:36:34 +0600

    Small Visual Inprovements

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -5,11 +5,22 @@ const Checkout = () => {
     const course = useLoaderData();
     //console.log(course);
     return (
-        <div>
-            <h2>Default Title</h2>
-            {
-                course.id && <h2>{course.title}</h2>
-            }
+        <div className="hero min-h-screen bg-base-200">
+            <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">CheckOut</h1>
+      <p className="mb-5">Provident cupid In deleniti eaque aut repudian.</p>
+      <div className="card w-96 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 shadow-xl text-neutral-content">
+        <div className="card-body items-center text-center">
+            <h2 className="card-title">{course.title}</h2>
+            <p>Cousre Fee: ${course.fees} || Duration: {course.duration}hr</p>
+            <div className="card-actions justify-end">
+            <button className="btn btn-primary">Confirm</button>
+            <button className="btn btn-ghost">Cancel</button>
+            </div>
+        </div>
+        </div>
+      </div>
+            
         </div>
     );
 };
--- src/components/Enroll/Enroll.js
+++ src/components/Enroll/Enroll.js
@@ -13,7 +13,7 @@ const Enroll = () => {
                 <div className=" flex-col lg:flex-row-reverse">
                     <div className="carousel w-full mb-5">
                     <div id="slide1" className="carousel-item relative w-full">
-                    <img src="https://placeimg.com/800/200/arch" className="w-full" />
+                    <img src="https://placeimg.com/800/200/arch" className="w-full" alt='slider' />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     </div>
                     </div>
