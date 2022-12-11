c24367265da86dcd9b656c6acba3dd6b90e04022
d3v-tushar <d3v.tushar@gmail.com>
2022-12-11 09:13:32 +0600

    Dark Mode Added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import { Link, NavLink } from 'react-router-dom';
 import { AuthContext } from '../../context/UserContext';
 import './Header.css';
 
-const Header = () => {
+const Header = ({darkMode, setDarkMode}) => {
     const {user, logOut} = useContext(AuthContext);
 
     //User Log Out
@@ -12,6 +12,15 @@ const Header = () => {
         logOut()
         .then(() => console.log('User Logged Out'))
         .catch(error => console.error(error.message))
+    };
+
+    const handleDarkMode = (e) =>{
+        if(e.target.checked){
+            setDarkMode(!darkMode)
+        }
+        else{
+            setDarkMode(false);
+        }
     };
     return (
         <>
@@ -36,7 +45,7 @@ const Header = () => {
                     </li>
                     </ul>
                     </div>
-                    <div className='site-logo'><Link to='/' className="btn btn-ghost normal-case font-bold text-2xl"><img src="https://i.ibb.co/TMJGXH5/logo.png" alt="site logo" /></Link></div>
+                    <div className='site-logo'><Link to='/' className="btn btn-ghost normal-case font-bold text-3xl"><img src="https://i.ibb.co/TMJGXH5/logo.png" className='hidden lg:grid' alt="site logo" /><p className='lg:hidden'><i><span className='text-gray-400'>B</span>B!</i></p></Link></div>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal p-0">
@@ -56,10 +65,10 @@ const Header = () => {
                 </div>
                 {/* Exp */}
                 <div className="navbar-end">
-                <label className="swap swap-rotate lg:grid hidden">
+                <label className="swap swap-rotate mx-2">
   
                 {/* <!-- this hidden checkbox controls the state --> */}
-                <input type="checkbox" />
+                <input onClick={handleDarkMode} type="checkbox" />
                 
                 {/* <!-- sun icon --> */}
                 <svg className="swap-on fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/></svg>
@@ -70,7 +79,7 @@ const Header = () => {
                 </label>
                 <div className="dropdown dropdown-end">
                         <Link to='/profile'>
-                        <div className="w-10 rounded-full mx-2 hover:tooltip hover:tooltip-open hover:tooltip-bottom" data-tip={user?.displayName || 'User Name'}>
+                        <div className="hidden lg:grid w-10 rounded-full mx-2 hover:tooltip hover:tooltip-open hover:tooltip-bottom" data-tip={user?.displayName || 'User Name'}>
                             <img className="h-10 rounded-full" src={user?.photoURL ? user.photoURL : "https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"} alt='dp' />
                         </div>
                         </Link>
--- src/layouts/Main/Main.js
+++ src/layouts/Main/Main.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 
 const Main = () => {
+    const [darkMode, setDarkMode] = useState(false);
     return (
-        <div>
-            <Header></Header>
+        <div className={darkMode ? "dark" : "light"}>
+            <Header darkMode={darkMode} setDarkMode={setDarkMode}></Header>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
